b68977ab58b14debf12dba33cbc1018fbd4e5d07
Blake A. Long <blake.a.long@gmail.com>
2020-10-13 12:16:50 -0600

    using JoinPoint object for method signature information

--- src/com/blakelong/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/blakelong/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,8 +1,9 @@
 package com.blakelong.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -14,8 +15,15 @@ public class MyDemoLoggingAspect {
 	AOPDeclarations aopDeclarations = new AOPDeclarations();
 	
 	@Before("com.blakelong.aopdemo.aspect.AOPDeclarations.forDaoPackageNoGetterAndSetters()")
-	public void beforeAddAccountAdvice() {
+	public void beforeAddAccountAdvice(JoinPoint joinPoint) {
 		System.out.println("\n===> Executing @Before advice on method");
+		
+		// display method signature
+		MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+		
+		System.out.println("Method Signature: " + methodSignature);
+		
+		// display method arguments
 	}
 	
 }
