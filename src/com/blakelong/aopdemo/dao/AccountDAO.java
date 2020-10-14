2e58ad518621c6dd758d5a86f32324389e9c6680
Blake A. Long <blake.a.long@gmail.com>
2020-10-14 10:48:40 -0600

    updates to Account and AccountDAO

--- src/com/blakelong/aopdemo/dao/AccountDAO.java
+++ src/com/blakelong/aopdemo/dao/AccountDAO.java
@@ -1,5 +1,8 @@
 package com.blakelong.aopdemo.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.blakelong.aopdemo.Account;
@@ -14,11 +17,27 @@ public class AccountDAO {
 		System.out.println(getClass() + ": Doing my db work: adding an account");
 	}
 	
+	public List<Account> findAccounts() {
+		List<Account> accounts = new ArrayList<>();
+		
+		// create sample accounts
+		Account acc1 = new Account("Blake", "Silver");
+		Account acc2 = new Account("Wes", "Gold");
+		Account acc3 = new Account("John", "Platinum");
+		
+		// add them to our accounts list
+		accounts.add(acc1);
+		accounts.add(acc2);
+		accounts.add(acc3);
+		
+		return accounts;
+	}
+	
 	public boolean doWork() {
 		System.out.println(getClass() + ": doWork()");
 		return false;
 	}
-
+	
 	public String getName() {
 		System.out.println(getClass() + ": getName() called");
 		return name;
