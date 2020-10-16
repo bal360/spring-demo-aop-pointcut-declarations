b7f949f75db6d10765e769f765640a9e5260dfbd
Blake A. Long <blake.a.long@gmail.com>
2020-10-16 12:57:34 -0600

    handled exception with try/catch in @Around advice

--- src/com/blakelong/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/blakelong/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -37,7 +37,17 @@ public class MyDemoLoggingAspect {
 		long begin = System.currentTimeMillis();
 		
 		// execute the method
-		Object result = proceedingJoinPoint.proceed();
+		Object result = null;
+		
+		try {
+			result = proceedingJoinPoint.proceed();
+		} catch (Exception e) {
+			// log the exception
+			System.out.println("Warning: " + e.getMessage());
+			
+			// give user a custom message
+			result = "Major accident! But no worries, private helicopter on the way";
+		}
 		
 		// get end timestamp
 		long end = System.currentTimeMillis();
