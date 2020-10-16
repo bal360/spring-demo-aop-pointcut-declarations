f5b9b41de102a9ffd8b72416a0dfbc41b7944b7b
Blake A. Long <blake.a.long@gmail.com>
2020-10-15 18:42:56 -0600

    TrafficFortuneService for @Around demo

---
+++ src/com/blakelong/aopdemo/AroundDemoApp.java
@@ -0,0 +1,44 @@
+package com.blakelong.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.blakelong.aopdemo.dao.AccountDAO;
+
+public class AroundDemoApp {
+	
+	public static void main(String[] args) {
+		
+		// read spring config class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from the spring container
+		AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		// call findAccounts()
+		List<Account> accounts = null;
+		
+		try {
+			// add boolean flag to simulate exceptions
+			boolean tripWire = false;
+			accounts = accountDAO.findAccounts(tripWire);
+			
+		} catch (Exception e) {
+			System.out.println("\n\nMain Program . . . caught exception: " + e);
+		}
+			
+
+		// display account
+		System.out.println("\n\nMain Program: AfterReturningDemoApp");
+		System.out.println("-----------------------");
+		
+		System.out.println(accounts);
+		
+		System.out.println("\n");
+		
+		// close the context
+		context.close();
+		
+	}
+}
